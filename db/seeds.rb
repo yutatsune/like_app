d8ead1726c03514189a442c86e295b75e6533d25
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-19 20:50:42 +0900

    初期データの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ログイン時に使用する「メールアドレス」と「パスワード」が分かりやすいよう一番上に配置
+email = 'test@example.com'
+password = 'password'
+
+# users テーブルを再生成（関連付いたテーブルを含む）
+ActiveRecord::Base.connection.execute('TRUNCATE TABLE users RESTART IDENTITY CASCADE')
+
+# 以下，開発用の初期データ
+user1 = User.create!(email: 'satou@example.com', password: 'password')
+user2 = User.create!(email: 'suzuki@example.com', password: 'password')
+user3 = User.create!(email: 'tanaka@example.com', password: 'password')
+
+user2.posts.create!(content: '沖縄そば')
+user1.posts.create!(content: '海ぶどう')
+user3.posts.create!(content: 'ゴーヤーチャンプルー')
+user3.posts.create!(content: 'サーターアンダギー')
+user1.posts.create!(content: 'タコライス')
+
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: email, password: password)
+
+puts '初期データの投入に成功しました！'
