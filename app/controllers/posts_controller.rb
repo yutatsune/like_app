d4c1bb1fca30d6939aaa46e8d96d378f293d645f
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-19 23:19:34 +0900

    Postコントローラーの作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
+
+  def index
+    @posts = Post.includes(:user).order(:created_at)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.create!(post_params)
+    redirect_to post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit; end
+
+  def update
+    @post.update!(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+    redirect_to root_path, alert: '権限がありません' if @post.nil?
+  end
+end
